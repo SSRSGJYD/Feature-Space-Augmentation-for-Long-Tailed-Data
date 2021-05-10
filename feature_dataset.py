5919d557ec50fb8ff00e79b7d63731625749b11a
zhouzp16 <zhouzp16@163.com>
2021-05-10 16:32:07 +0800

    feature dataset

---
+++ feature_dataset.py
@@ -0,0 +1,81 @@
+import json
+import numpy as np
+import random
+import torch
+from torch.utils.data import Dataset
+
+
+class FeatureDataset(Dataset):
+    """This class is the dataset in stage 2 and 3."""
+
+    def __init__(self, config):
+        self.Nt = config.Nt
+        self.Na = config.Na
+        self.Nf = config.Nf
+        self.ts = config.ts
+        self.tg = config.tg
+        self.gamma = config.gamma
+
+        self.head_classes = json.load(config.head_classes)
+        self.class_samples = json.load(config.class_samples)
+        self.confusing_head_classes = json.load(config.confusing_head_classes)
+        self.head_class_samples = []
+        self.tail_class_samples = []
+        for c, samples in self.class_samples:
+            if c in self.head_classes:
+                self.head_class_samples += samples
+            else:
+                self.tail_class_samples += samples
+
+    def read_feature(self, sample, need_cam=True):
+        feature = np.load('')
+        cam = np.load('') if need_cam else None
+        return feature, cam
+
+    def fusion_feature(self, tail_sample):
+        # feature fusion
+        tail_feature, tail_cam = self.read_feature(tail_sample)
+        tail_feature = np.where(tail_cam > self.ts, tail_feature, 0)
+        tail_feature = np.mean(tail_feature, axis=(1,2))
+        tail_features = [tail_feature]
+
+        tail_class = tail_sample['class']
+        confusing_classes = self.confusing_head_classes[tail_class]
+        for head_class in confusing_classes:
+            # sample one sample from each of the Na head classes
+            confusing_sample = random.sample(self.class_samples[head_class], 1)     
+            head_feature, head_cam = self.read_feature(confusing_sample)
+            head_feature = np.where(head_cam < self.tg, head_feature, 0)
+            head_feature = np.mean(head_feature, axis=(1,2))
+            fusion_feature = self.gamma * tail_feature + (1-self.gamma) * head_feature
+            tail_features.append(fusion_feature)
+        return tail_features
+ 
+    def __len__(self):
+        return len(self.tail_class_samples)
+
+    def __getitem__(self, idx):
+        batch_features = self.fusion_feature(self.tail_class_samples[idx])
+        label = torch.zeros(2*(1+self.Na), dtype=torch.long)
+
+        # head class samples
+        idx = 1 + self.Na
+        head_samples = random.sample(self.head_class_samples, 1+self.Na)
+        for head_sample in head_samples:
+            head_feature, _ = self.read_feature(head_sample, False)
+            batch_features.append(head_feature)
+            label[idx] = head_sample['class']
+            idx += 1
+        
+        return batch_features, label
+
+    def collate_fn(self, batch):
+        features = []
+        labels = []
+        for batch_features, label in batch:
+            features += batch_features
+            labels.append(label)
+
+        input_tensor = torch.from_numpy(np.stack(features)).contiguous()
+        label = torch.cat(labels)
+        return input_tensor, label
---
+++ util.py
@@ -0,0 +1,39 @@
+import multiprocessing
+
+def multiprocess_in_sequence(func, param_list, workers=10):
+    param_data = [[] for _ in range(workers)]
+    c_worker = 0
+    for i, param in enumerate(param_list):
+        param_data[c_worker].append((i,param))
+        c_worker = (c_worker + 1) % workers
+
+    q = multiprocessing.Queue()
+    q.cancel_join_thread()
+    count = 0
+
+    def worker(func, param_part_list):
+        for i, param in param_part_list:
+            try:
+                if isinstance(param, list):
+                    data = func(*param)
+                else:
+                    data = func(param)
+            except:
+                continue
+            q.put((i, data))
+
+    for i in range(workers):
+        w = multiprocessing.Process(
+            target=worker,
+            args=(func, param_data[i]))
+        w.daemon = False
+        w.start()
+
+    data_list = [None for _ in range(len(param_list))]
+
+    while count < len(param_list):
+        i, data = q.get()
+        data_list[i] = data
+        count += 1
+
+    return data_list
